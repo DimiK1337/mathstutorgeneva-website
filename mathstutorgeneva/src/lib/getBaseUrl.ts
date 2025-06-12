837c48263082c82de398c14f3336b456f6b831d1
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-12 21:26:00 +0200

    getBaseUrl.ts: Return the window origin directly and use the rest as a fallback

--- mathstutorgeneva/src/lib/getBaseUrl.ts
+++ mathstutorgeneva/src/lib/getBaseUrl.ts
@@ -1,5 +1,8 @@
 
 
-export function getBaseUrl(): string {
-    return process.env.NEXT_PUBLIC_SITE_URL || 'https://mathstutorgeneva.vercel.app';
+export function getBaseUrl() {
+    if (typeof window !== "undefined") {
+        return window.location.origin;
+    }
+    return process.env.NEXT_PUBLIC_SITE_URL || "https://mathstutorgeneva.ch";
 }
